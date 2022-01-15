564058b929f1845a2f452f0d31a6b186c578c42a
Anthony Djoewanda <16738612+tonychuaco@users.noreply.github.com>
2022-01-14 23:09:52 -0500

    Added endpoint /json

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # [Introduction to the Basic Node and Express Challenges](https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/)
+
+- [x] "Hello World" should be in the console
+- [x] Your app should serve the string 'Hello Express'
+- [x] Your app should serve the file views/index.html
+- [x] Your app should serve asset files from the /public directory to the /public path
+- [] The endpoint /json should serve the json object {"message": "Hello json"}
--- myApp.js
+++ myApp.js
@@ -1,39 +1,22 @@
 var express = require('express');
 var app = express();
 
+// console.log("Hello World")
 
+// app.get('/', function(req, res) {
+//   res.send('Hello Express')
+// })
 
+app.use('/public', express.static(__dirname + '/public'))
 
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html')
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/json", (req, res) => {
+  res.json({
+    "message": "Hello json"
+  });
+});
 
  module.exports = app;
